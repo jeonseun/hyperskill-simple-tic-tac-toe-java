42792a8dbf0410aa63d8a21df384558e15da759b
jeonseun <jeonseun1234@gmail.com>
2023-11-12 23:39:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Tic-Tac-Toe (Java)
+
+hyperskill java project
+
+## 개요
+
+## 환경
+
+- Java 17
+- IntelliJ IDEA 2023.2.3 (Ultimate Edition)
+
+## 배운 것
---
+++ Simple Tic-Tac-Toe (Java)/task/src/tictactoe/Main.java
@@ -0,0 +1,147 @@
+package tictactoe;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] field = new char[][]{
+                {' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}
+        };
+
+        showField(field);
+
+        boolean isXTurn = true;
+        int turnCount = 0;
+
+        while (true) {
+            int y = readUserInput(scanner);
+            if (y == -1) {
+                continue;
+            }
+
+            int x = readUserInput(scanner);
+            if (x == -1) {
+                continue;
+            }
+
+            if (!isValidCoordinates(x, y)) {
+                continue;
+            }
+
+            if (!isValidPosition(field, x, y)) {
+                continue;
+            }
+
+            if (isXTurn) {
+                field[y - 1][x - 1] = 'X';
+            } else {
+                field[y - 1][x - 1] = 'O';
+            }
+            turnCount++;
+            isXTurn = !isXTurn;
+
+            showField(field);
+
+            boolean isXWin;
+            boolean isOWin;
+
+            isXWin = checkWinner(field, 'X');
+            isOWin = checkWinner(field, 'O');
+
+            if (isXWin) {
+                System.out.println("X wins");
+                break;
+            } else if (isOWin) {
+                System.out.println("O wins");
+                break;
+            } else if (turnCount == 9) {
+                System.out.println("Draw");
+                break;
+            }
+        }
+
+    }
+
+    private static void showField(char[][] field) {
+        String grid = """
+                ---------
+                | %c %c %c |
+                | %c %c %c |
+                | %c %c %c |
+                ---------""";
+
+        System.out.printf(grid + "%n",
+                field[0][0], field[0][1], field[0][2],
+                field[1][0], field[1][1], field[1][2],
+                field[2][0], field[2][1], field[2][2]
+        );
+    }
+
+    private static int readUserInput(Scanner scanner) {
+        int input;
+        try {
+            input = scanner.nextInt();
+            return input;
+        } catch (InputMismatchException e) {
+            System.out.println("You should enter numbers!");
+            scanner.nextLine();
+            return -1;
+        }
+    }
+
+    private static boolean isValidCoordinates(int x, int y) {
+        if (y < 1 || y > 3 || x < 1 || x > 3) {
+            System.out.println("Coordinates should be from 1 to 3!");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isValidPosition(char[][] field, int x, int y) {
+        if (field[y - 1][x - 1] != ' ') {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkWinner(char[][] field, char player) {
+        return checkVertical(field, player) || checkHorizontal(field, player) || checkDiagonals(field, player);
+    }
+
+    private static boolean checkVertical(char[][] field, char player) {
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i] == field[1][i] && field[1][i] == field[2][i]) {
+                if (field[0][i] == player) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkHorizontal(char[][] field, char player) {
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0] == field[i][1] && field[i][1] == field[i][2]) {
+                if (field[i][0] == player) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean checkDiagonals(char[][] field, char player) {
+        if (field[0][0] == field[1][1] && field[1][1] == field[2][2] ||
+                field[2][0] == field[1][1] && field[1][1] == field[0][2]) {
+            return field[1][1] == player;
+        }
+        return false;
+    }
+
+}
